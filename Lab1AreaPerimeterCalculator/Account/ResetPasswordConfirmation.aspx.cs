ab509aafbb50e5f1aa1f6a7a1a3fe04cc18b6c2b
moyang9192 <moyang9192@gmail.com>
2016-09-26 14:48:59 -0400

    Add project files.

---
+++ Lab1AreaPerimeterCalculator/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Lab1AreaPerimeterCalculator.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Lab1AreaPerimeterCalculator/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab1AreaPerimeterCalculator.Startup))]
+namespace Lab1AreaPerimeterCalculator
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
